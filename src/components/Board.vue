a0a4dcf4b6fc50aaffe8fe8b5374be5dce2df31b
Matthew Froggatt <mattfr@live.ca>
2020-02-05 21:46:23 -0500

    Created Vue app and messed with it.

---
+++ src/components/Board.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="board">
+    <Octothorpe msg="hello"/>
+  </div>
+</template>
+
+<script>
+import Octothorpe from './Octothorpe.vue'
+
+export default {
+  name: 'Board',
+  props: {
+    msg: String
+  },
+  components: {
+    Octothorpe
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#board {
+  border-radius: 10px;
+  border-style: solid;
+  background-color: yellow;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Octothorpe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="octothorpe">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Octothorpe',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  padding: 100px;
+}
+h3 {
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
