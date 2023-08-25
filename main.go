474f30d70542f6e92e2ec930168831717ee34f89
Matthew John Froggatt <mattfr@live.ca>
2023-08-25 16:02:05 -0400

    Send IDs

--- main.go
+++ main.go
@@ -1,28 +1,72 @@
 package main
+
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/websocket"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{}
+
+type Message struct {
+	Command string `json:"command"`
+	ID      string `json:"id"`
+}
+
+var connectedIds = make(map[int]bool)
+
+func generateId() int {
+	for {
+		id := rand.Int()
+		if connectedIds[id] == false {
+			connectedIds[id] = true
+			return id
+		}
+	}
+}
 
 func reader(connection *websocket.Conn) {
 	for {
 		_, messageBytes, err := connection.ReadMessage()
 		if err != nil {
-			log.Println(err)
+			log.Println("Error reading websocket message", err)
 			return
 		}
 		// print out that message for clarity
+
 		fmt.Println(string(messageBytes))
 
-		err = connection.WriteMessage(1, []byte("yo"))
+		var v Message
+		err = json.Unmarshal(messageBytes, &v)
 		if err != nil {
-			log.Println(err)
+			log.Println("Error parsing JSON", err)
+			return
 		}
 
+		var response Message
+
+		switch v.Command {
+		case "requestId": // send request id
+			response = Message{Command: "respondId", ID: strconv.Itoa(generateId())}
+
+		default:
+			log.Println("Unknown command:", v.Command)
+		}
+
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			log.Println("Error marshaling response:", err)
+			continue
+		}
+		err = connection.WriteMessage(websocket.TextMessage, responseBytes)
+		if err != nil {
+			log.Println("Error sending response:", err)
+		}
 	}
 }
 
